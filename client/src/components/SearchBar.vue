fc4e8c2324b2aa6b80f2a469f5bcb3eb81cc9e1d
carraes <carraeshb@gmail.com>
2023-05-02 16:25:14 -0300

    feat: search bar component, lifts state to App component

---
+++ client/src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const searchText = ref("");
+const emit = defineEmits(["search"]);
+
+const onSearch = () => {
+  emit("search", searchText.value);
+};
+</script>
+
+<template>
+  <input
+    type="text"
+    class="text-black"
+    v-model="searchText"
+    @keyup.enter="onSearch"
+  />
+  <button @click="onSearch">Search</button>
+</template>
