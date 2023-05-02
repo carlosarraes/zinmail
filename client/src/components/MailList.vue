7eae3881bb1a17068cd18d69a9268a2894bb9a0f
carraes <carraeshb@gmail.com>
2023-05-02 20:52:18 -0300

    refactor: move modal to its own component

--- client/src/components/MailList.vue
+++ client/src/components/MailList.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import type { Email } from "../interfaces/Email";
+import type { Email, SingleMail } from "../interfaces/Email";
+import Modal from "./Modal.vue";
 import { ref } from "vue";
+
 const props = defineProps<{
   mails: Email[];
 }>();
 
 const showMail = ref(false);
-const singleMailContent = ref({
+const singleMailContent = ref<SingleMail>({
   from: "",
   to: "",
   subject: "",
@@ -19,50 +21,18 @@ const toggleMail = (mail?: string) => {
   if (mailContent) {
     singleMailContent.value = mailContent;
   }
-  console.log(singleMailContent.value);
 };
 </script>
 
 <template>
-  <div
-    v-if="showMail"
-    class="absolute w-full h-full bg-black bg-opacity-70 z-10 flex items-center justify-center"
-  >
-    <div class="w-8/12 bg-white rounded-md relative">
-      <div class="absolute top-0 right-0 m-4">
-        <button
-          @click="toggleMail('close')"
-          class="text-2xl text-black rounded-full bg-red-500 w-8 h-8"
-        >
-          &times;
-        </button>
-      </div>
-      <div class="flex flex-col m-2 text-left p-4 overflow-y-auto max-h-96">
-        <p class="text-black text-sm">From: {{ singleMailContent.from }}</p>
-        <p class="text-black text-sm">
-          To:
-          <span v-if="singleMailContent.to.split(',').length > 2">
-            {{ singleMailContent.to.split(",")[0].trim() }},
-            {{ singleMailContent.to.split(",")[1].trim() }}
-            <span class="text-gray-600">
-              and {{ singleMailContent.to.split(",").length - 2 }} more...
-            </span>
-          </span>
-          <span v-else>
-            {{ singleMailContent.to }}
-          </span>
-        </p>
-        <p class="text-black text-sm">
-          Subject: {{ singleMailContent.subject }}
-        </p>
-        <p class="text-black text-sm mt-4">Content:</p>
-        <p class="text-black text-sm mt-2">
-          {{ singleMailContent.content }}
-        </p>
-      </div>
-    </div>
+  <div class="modal-wrapper" v-show="showMail">
+    <Modal
+      :showMail="showMail"
+      :singleMailContent="singleMailContent"
+      :toggleMail="toggleMail"
+    />
   </div>
-  <div class="w-full mx-auto table-wrapper">
+  <section class="w-full mx-auto table-wrapper">
     <table class="w-[1200px] divide-y divide-gray-200">
       <thead class="bg-gray-50 min-w-full sticky top-0 z-0 shadow-md">
         <tr>
@@ -88,7 +58,7 @@ const toggleMail = (mail?: string) => {
             scope="col"
             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
           >
-            Highlight
+            Matched Content
           </th>
         </tr>
       </thead>
@@ -120,7 +90,7 @@ const toggleMail = (mail?: string) => {
         </tr>
       </tbody>
     </table>
-  </div>
+  </section>
 </template>
 
 <style scoped>
@@ -128,5 +98,14 @@ const toggleMail = (mail?: string) => {
   max-height: calc(100vh - 4rem);
   overflow-y: scroll;
   padding-bottom: 2rem;
+}
+
+.modal-wrapper {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 1;
 }
 </style>
---
+++ client/src/components/Modal.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import type { SingleMail } from "../interfaces/Email";
+
+defineProps<{
+  showMail: boolean;
+  singleMailContent: SingleMail;
+  toggleMail: (mail?: string) => void;
+}>();
+</script>
+
+<template>
+  <div
+    v-if="showMail"
+    class="absolute w-full h-full bg-black bg-opacity-70 z-10 flex items-center justify-center"
+    @click="toggleMail('close')"
+  >
+    <div class="w-[800px] h-[800px] bg-white rounded-md relative" @click.stop>
+      <div class="absolute top-0 right-0 m-4">
+        <button
+          @click="toggleMail('close')"
+          class="text-2xl text-black rounded-full bg-red-500 w-8 h-8"
+        >
+          &times;
+        </button>
+      </div>
+      <div class="flex flex-col m-2 text-left p-4 overflow-y-auto h-full">
+        <p class="text-black text-sm">From: {{ singleMailContent.from }}</p>
+        <p class="text-black text-sm">
+          To:
+          <span v-if="singleMailContent.to.split(',').length > 2">
+            {{ singleMailContent.to.split(",")[0].trim() }},
+            {{ singleMailContent.to.split(",")[1].trim() }}
+            <span class="text-gray-600">
+              and {{ singleMailContent.to.split(",").length - 2 }} more...
+            </span>
+          </span>
+          <span v-else>
+            {{ singleMailContent.to }}
+          </span>
+        </p>
+        <p class="text-black text-sm">
+          Subject: {{ singleMailContent.subject }}
+        </p>
+        <p class="text-black text-sm mt-4">Content:</p>
+        <p class="text-black text-sm mt-2">
+          {{ singleMailContent.content }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
