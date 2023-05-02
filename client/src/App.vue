0c79d43ed53c1c1bde1f75e9bee8b8c54ed0c62d
carraes <carraeshb@gmail.com>
2023-05-02 16:24:14 -0300

    feat: App component, gets data from backend, sends it to mailist

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,33 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import SearchBar from "./components/SearchBar.vue";
+import type { Email } from "./interfaces/Email";
+import MailList from "./components/MailList.vue";
+
+const mails = ref<Email[]>([]);
+
+const handleSearch = async (searchTerm: string) => {
+  const response = await fetch("http://localhost:8080/search", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ searchTerm }),
+  });
+
+  const { emails } = await response.json();
+  mails.value = emails;
+};
+</script>
+
 <template>
-  <h1>Hello</h1>
+  <h1 class="header">Hello</h1>
+  <SearchBar @search="handleSearch" />
+  <MailList :mails="mails" />
 </template>
+
+<style>
+body {
+  overflow-y: hidden;
+}
+</style>
