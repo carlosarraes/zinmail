a68c6c4bd70eb49d951dcea45d04fa6143b8e631
carraes <carraeshb@gmail.com>
2023-06-15 01:09:44 -0300

    feat: trying some indexing

--- data/main.go
+++ data/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/jhillyerd/enmime"
 )
@@ -32,55 +32,94 @@ type EmailData struct {
 	XFilename       string `json:"x_filename"`
 }
 
+type BulkData struct {
+	Index   string      `json:"index"`
+	Records []EmailData `json:"records"`
+}
+
 func main() {
+	log.Println("Start indexing...")
+
+	var records []EmailData
+	var m sync.Mutex
+	var wg sync.WaitGroup
+
 	err := filepath.Walk("./enron_mail_20110402/maildir/", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			content, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
-			mime, err := enmime.ReadEnvelope(bytes.NewReader(content))
-			if err != nil {
-				return err
-			}
-			emailData := EmailData{
-				From:            mime.GetHeader("From"),
-				To:              mime.GetHeader("To"),
-				Subject:         mime.GetHeader("Subject"),
-				Content:         mime.Text,
-				MessageID:       mime.GetHeader("Message-ID"),
-				Date:            mime.GetHeader("Date"),
-				ContentType:     mime.GetHeader("Content-Type"),
-				MimeVersion:     mime.GetHeader("Mime-Version"),
-				ContentEncoding: mime.GetHeader("Content-Transfer-Encoding"),
-				XFrom:           mime.GetHeader("X-From"),
-				XTo:             mime.GetHeader("X-To"),
-				Xcc:             mime.GetHeader("X-cc"),
-				Xbcc:            mime.GetHeader("X-bcc"),
-				XFolder:         mime.GetHeader("X-Folder"),
-				XOrigin:         mime.GetHeader("X-Origin"),
-				XFilename:       mime.GetHeader("X-Filename"),
-			}
-			jsonData, err := json.Marshal(emailData)
-			if err != nil {
-				return err
-			}
-			sendToZincSearch(jsonData)
+			wg.Add(1)
+			go func(p string) {
+				defer wg.Done()
+				emailData, err := processFile(p)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				m.Lock()
+				records = append(records, emailData)
+				m.Unlock()
+			}(path)
 		}
 		return nil
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	wg.Wait()
+
+	sendBulkToZincSearch(records)
+
+	log.Println("Finished indexing")
 }
 
-func sendToZincSearch(jsonData []byte) {
-	req, err := http.NewRequest("POST", "http://localhost:4080/api/emails/_doc", bytes.NewReader(jsonData))
+func processFile(path string) (EmailData, error) {
+	content, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatal(err)
+		return EmailData{}, err
+	}
+	mime, err := enmime.ReadEnvelope(bytes.NewReader(content))
+	if err != nil {
+		return EmailData{}, err
+	}
+	return EmailData{
+		From:            mime.GetHeader("From"),
+		To:              mime.GetHeader("To"),
+		Subject:         mime.GetHeader("Subject"),
+		Content:         mime.Text,
+		MessageID:       mime.GetHeader("Message-ID"),
+		Date:            mime.GetHeader("Date"),
+		ContentType:     mime.GetHeader("Content-Type"),
+		MimeVersion:     mime.GetHeader("Mime-Version"),
+		ContentEncoding: mime.GetHeader("Content-Transfer-Encoding"),
+		XFrom:           mime.GetHeader("X-From"),
+		XTo:             mime.GetHeader("X-To"),
+		Xcc:             mime.GetHeader("X-cc"),
+		Xbcc:            mime.GetHeader("X-bcc"),
+		XFolder:         mime.GetHeader("X-Folder"),
+		XOrigin:         mime.GetHeader("X-Origin"),
+		XFilename:       mime.GetHeader("X-Filename"),
+	}, nil
+}
+
+func sendBulkToZincSearch(records []EmailData) {
+	bulkData := BulkData{
+		Index:   "emails",
+		Records: records,
+	}
+
+	jsonData, err := json.Marshal(bulkData)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:4080/api/_bulkv2", bytes.NewReader(jsonData))
+	if err != nil {
+		log.Println(err)
+		return
 	}
 	req.SetBasicAuth("admin", "password")
 	req.Header.Set("Content-Type", "application/json")
@@ -88,13 +127,14 @@ func sendToZincSearch(jsonData []byte) {
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	defer resp.Body.Close()
-	log.Println(resp.StatusCode)
-	body, err := ioutil.ReadAll(resp.Body)
+
+	_, err = io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
-	fmt.Println(string(body))
 }
